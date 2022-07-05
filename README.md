fd89db9769a1f13e0b5e20a49a827477a4f228f1
Adnan <adnan66b@gmail.com>
2022-07-05 14:41:33 +0300

    Showing solved problems in README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+
 # Codeforces
 A repository to keep track of my solutions to problems in Codeforces.
+# Solved Problems
+| # | Title | lang | Submitted |
+|---| ----- | ---- | --------- |
+1 | [A - Parkway Walk](https://codeforces.com/contest/1697/problem/A) | Java 8 | Jun/19/2022 14:55 |
+2 | [A - Chess Tourney](https://codeforces.com/contest/845/problem/A) | Java 8 | Jan/16/2022 06:51 |
+3 | [C - Paint the Array](https://codeforces.com/contest/1618/problem/C) | Java 8 | Jan/15/2022 20:27 |
---
+++ scraper.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+
+# function to extract html document from Codeforces
+def getHTMLdocument(url):
+  response = requests.get(url)
+  return response.text
+
+def getSolvedProblems():
+  html_document = getHTMLdocument("https://codeforces.com/submissions/Adxnan10")
+  soup = BeautifulSoup(html_document, 'html.parser')
+  solved_problems = [] # It will have this format: [ {"Source": "", "When": "" }, {"Source": "", "When": "" }  ]
+  problems = soup.table.find_all('tr')
+  for problem in problems: 
+    if problem.find(class_ ="verdict-accepted"):
+      problem_info = problem.find_all(recursive=False)
+      problem_object = {
+        # "source" : problem_info[0].a['href'], I might use it instead of pushing my code locally.
+        "date" : problem_info[1].getText().strip(),
+        "link" : "https://codeforces.com" + problem_info[3].a['href'],
+        "name" : problem_info[3].a.getText().strip(),
+        "lang" : problem_info[4].getText().strip(),
+      }
+      solved_problems.append(problem_object)
+  return solved_problems
+
+def appendToReadMe():
+  solved_problems = getSolvedProblems()
+  intro = """
+# Codeforces
+A repository to keep track of my solutions to problems in Codeforces.
+# Solved Problems
+| # | Title | lang | Submitted |
+|---| ----- | ---- | --------- |
+"""
+  with open("README.md", "w") as README:
+    README.write(intro)
+    for index, problem in enumerate(solved_problems):
+      problem_row = f"{index + 1} | [{problem['name']}]({problem['link']}) | {problem['lang']} | {problem['date']} |"
+      README.write(problem_row + "\n")
+      
+appendToReadMe()
